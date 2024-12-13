7ea0761bdcba512509413efe7376106aeb2f056d
suchmolin <suchmolin11@gmail.com>
2024-12-13 18:05:49 -0400

    navbar ajuste programas dropdown

--- src/components/shared/SubMenu/page.jsx
+++ src/components/shared/SubMenu/page.jsx
@@ -1,6 +1,24 @@
+import { Dropdown } from "flowbite-react"
 import Link from "next/link"
+import { useState } from "react"
+import { RiArrowDropDownLine } from "react-icons/ri"
 
 export default function SubMenu({ selected, setSelected }) {
+  const [progOpened, setProgOpened] = useState(false)
+  const customTheme = {
+    arrowIcon: "ml-1 h-4 w-4",
+    content: "py-1 focus:outline-none ",
+    floating: {
+      item: {
+        base: "flex w-full cursor-pointer items-center justify-start px-4 py-2 text-white hover:text-azulBooster hover:bg-white focus:bg-gray-100 focus:outline-none",
+      },
+      style: {
+        auto: "bg-azulClaroBooster text-white mt-3 -ml-2 w-[167px] shadow-none",
+      },
+      target: "w-fit",
+    },
+    inlineWrapper: "flex items-center",
+  }
   const styleLI =
     "hover:text-azulBooster transition-all duration-300 cursor-pointer lg:text-center w-fit"
   return (
@@ -19,12 +37,44 @@ export default function SubMenu({ selected, setSelected }) {
           NOSOTROS
         </Link>
       </li>
+
       <li
-        className={`${styleLI} ${selected === "programas" ? "text-azulBooster" : ""}`}
+        onClick={() => setProgOpened(!progOpened)}
+        className={`lg:hidden ${styleLI} `}
       >
-        <Link href="/programas" onClick={(e) => setSelected("programas")}>
-          PROGRAMAS
-        </Link>
+        <button
+          className={`flex gap-2 items-center text-white ${progOpened ? "text-azulBooster" : ""}`}
+        >
+          PROGRAMAS <RiArrowDropDownLine className="text-3xl" />
+        </button>
+        <div className="text-xl">
+          {progOpened && (
+            <ul className="flex flex-col gap-4 mt-4 ml-4">
+              <li className="hover:text-azulBooster text-white transition-all duration-300 cursor-pointer">
+                <Link
+                  href="/boostermath"
+                  onClick={(e) => setSelected("boostermath")}
+                >
+                  Boostermath
+                </Link>
+              </li>
+              <li className="hover:text-azulBooster text-white transition-all duration-300 cursor-pointer">
+                <Link
+                  href="/boosterclub"
+                  onClick={(e) => setSelected("boosterclub")}
+                >
+                  Boosterclub
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </li>
+      <li className={`hidden lg:block ${styleLI} `}>
+        <Dropdown theme={customTheme} label="PROGRAMAS" inline>
+          <Dropdown.Item>Boostermath</Dropdown.Item>
+          <Dropdown.Item>Boosterclub</Dropdown.Item>
+        </Dropdown>
       </li>
       <li
         className={`${styleLI} ${selected === "blog" ? "text-azulBooster" : ""}`}
